e691c33146168a7cb6d4e6cfbe06b598804d420c
Brian <linbrian24@gmail.com>
2025-12-01 17:20:36 -0500

    cleanup

--- index.js
+++ index.js
@@ -2,19 +2,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello Worldssss!</h1>");
+  res.send("<h1>Hello World!</h1>");
 });
 
 const PORT = 8080;
 
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});
-
-process.on("SIGTERM", () => {
-  console.log("Received SIGTERM, shutting down...");
-  server.close(() => {
-    console.log("Server closed");
-    process.exit(0);
-  });
 });
