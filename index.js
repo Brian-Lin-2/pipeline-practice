620b9e1691d324bad37e4fe7b036aa6892f35d9e
Brian <linbrian24@gmail.com>
2025-12-01 16:14:34 -0500

    fix term

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello Worldsss!</h1>')
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Hello Worldsss!</h1>");
+});
 
-const PORT = 8080
+const PORT = 8080;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
+
+process.on("SIGTERM", () => {
+  console.log("Received SIGTERM, shutting down...");
+  server.close(() => {
+    console.log("Server closed");
+    process.exit(0);
+  });
+});
